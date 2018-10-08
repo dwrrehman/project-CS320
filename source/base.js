9f44c0819fdd8cd3ed40bb302d1ee4571ae61e5e
Daniel Rehman <dwrrehman@gmail.com>
2018-10-08 02:02:05 -0700

    completed base UI for the calulator, preparing to implement calulator algorithm code.

--- source/base.js
+++ source/base.js
@@ -1,29 +1,78 @@
-// this file contains the algorithmic code for doing unit conversion, as well as the view-switching code.
+// this file contains the algorithmic javascritpt code for the project, as well as some of the view-switching code.
 
+// converter display modes:
 const convert_displaymode = 0;
 const add_displaymode = 1;
 const list_displaymode = 2;
-
 var displaymode = convert_displaymode;
 
-var conversions = [];
 
-function unit_conversion(from_unit_name, to_unit_name, conversion_factor) {
-	this.from_unit_name = from_unit_name;
-	this.to_unit_name = to_unit_name;
-	this.conversion_factor = conversion_factor;
+
+
+
+//--------------------------- converter code: ---------------------------------
+
+var systemes = []; // array of array of units. each element in the outer array is a system, and each element in the system is a unit.
+// except its not an array of arrays, its a array of pair<name, array>. where name is the name of the systems, and array is an array of units.
+
+
+
+function system(given_systemname, given_units) {
+	this.systemname = given_name;
+	this.units = given_units;
 }
 
-function add_unit_conversion(conversion) {
-	if (conversion.from_unit_name === undefined) return;
-	if (conversion.to_unit_name === undefined) return;
-	if (conversion.conversion_unit_name === undefined) return;
-	conversions.push(conversion);
+function unit(given_unitname, given_definition) {
+	this.unitname = given_unitname;
+	this.definition = given_definition;
 }
 
-function get_all_current_conversions() {
-	return conversions;
+function add_new_unit_to_system(given_system, given_name, definition) {
+	for (i = 0; i < systemes.length; i++) {
+		if (systemes[i].systemname === given_system) {
+			systemes[i].units.push(unit(given_name, definition));
+		}
+	}
 }
+
+
+function add_new_system(given_systemname) {
+
+	systemes.push(system(given_systemname, []));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// calcilator stuff:
+
+
+var display_string = "";
+
+function receive(string) {
+
+	if (string.length == 1) {
+		display_string += string;
+	} else if (string == "clear"){
+		display_string = "";
+	} else {
+		display_string = "";
+	}
+	document.getElementById("calculator_display").innerText = display_string;
+}
+
+
+
+//// ---------------------------- view switching code ------------------------------
 
 
 // finds html file on server side, and retreives its data.
@@ -53,8 +102,13 @@ function draw() {
 	main();
 }
 
+// a function for each page that is run when the page finsihes loading.
 function main() {
 	if (displaymode === convert_displaymode) {		
+		document.getElementById("calculator_display").text = display_string;
+
+
+
 
 	} else if (displaymode === add_displaymode) {		
 
@@ -64,21 +118,63 @@ function main() {
 }	
 
 
-/// convert.html code:
+
+
+
+
+
+
+
+
+
+
+
+
+/// ------------------- main.html code: ---------------------------------
+
+
+
+
+
 
 function convert() {	
-	document.getElementById("value").innerHTML = "42";
-	document.getElementById("value").setAttribute("style", "font-size: 15px; font-family: Courier;");
+	let result = null;
+	let input = document.getElementById("converter_input").value;
+
+	if (input == "") {
+		document.getElementById("converter_result").value = result;
+		return;
+	}
+
+
+	result = Number(input) * Number(input);
+
+	//get the units the user picked.
+
+	var from = document.getElementById("from_unit").value;
+	var to = document.getElementById("to_unit").value;
+
+	console.log(from);
+	console.log(to);
+	
+	// do some magic to convert it based on the exising units
+
+	
+	document.getElementById("converter_result").value = result;
 }
 
-function reset() {
-	document.getElementById("value").innerHTML = "<button style=\"font-size: 10px;\" style=\"font-family: Courier;\" onclick=\"convert();\">Convert</button>";
-}
 
 
 
 
-/// add.html code:
+
+
+
+
+
+
+
+/// --------- add.html code: --------------          [OLD and invalid now]
 
 function validate(formula) {	
 	var result = true;

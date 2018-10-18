dee19fe349e9af49f7810444477404c46307a337
Daniel Rehman <dwrrehman@gmail.com>
2018-10-18 15:29:04 -0700

    clean changes

--- source/base.js
+++ source/base.js
@@ -10,6 +10,18 @@ var displaymode = convert_displaymode;
 
 
 
+
+const conversion_precision = 5;// digits past the decimal point.
+
+
+
+
+
+
+var bin_variables = [];
+var bin_constants = [];
+
+
 //--------------------------- converter code: ---------------------------------
 
 var systemes = []; // array of array of units. each element in the outer array is a system, and each element in the system is a unit.
@@ -35,14 +47,40 @@ function add_new_unit_to_system(given_system, given_name, definition) {
 	}
 }
 
-
 function add_new_system(given_systemname) {
-
 	systemes.push(system(given_systemname, []));
 }
 
 
 
+
+
+
+
+
+
+
+
+// variables and constants: bins :
+
+// a ds that could be a var or a const.
+function bin(given_name, given_data) {
+	this.given_name = given_name;
+	this.given_data = given_data;
+}
+
+
+// adders:
+
+function add_variable(given_name, given_data) {
+	if (given_name === undefined  && given_data === undefined) return;
+	bin_variables.push(bin(given_name, given_data));
+}
+
+function add_constant(given_name, given_data) {
+	if (given_name === undefined  && given_data === undefined) return;
+	bin_constants.push(bin(given_name, given_data));
+}
 
 
 
@@ -59,6 +97,35 @@ function add_new_system(given_systemname) {
 var display_string = "";
 
 function receive(string) {
+
+
+	if (string == "store") {
+		let bin_type = prompt("Specify the bin type: (variable/constant)", "variable");
+		let given_name = prompt("Specify the name of the bin: ");
+
+		let raw_data = document.getElementById("calculator_display").innerText;
+		let num_data = Number(raw_data);
+		if (isNaN(num_data)) {
+			alert("cannot store: " + raw_data);
+			return;
+		}
+		
+		if (bin_type == "variable") {
+			add_variable(given_name, num_data);
+			alert("Stored Successfully");
+		} else if (bin_type == "constant"){
+			add_constant(given_name, num_data);
+			alert("Stored Successfully");
+		} else {
+			alert("cannot use bin type: " + bin_type);			
+		}		
+		return;
+	}
+
+	else if (string == "load") {
+		console.log("found load");
+
+	}
 
 	if (string.length == 1) {
 		display_string += string;
@@ -113,7 +180,13 @@ function main() {
 	} else if (displaymode === add_displaymode) {		
 
 	} else if (displaymode === list_displaymode) {
-		
+		for (v = 0; v < bin_variables.length; v++) {
+			var n = bin_variables[v].given_name;
+			var d = bin_variables[v].given_data;
+
+			document.getElementById("text").innerText += n + " = " + d + "\n";
+
+		}
 	}
 }	
 
@@ -141,26 +214,31 @@ function convert() {
 	let result = null;
 	let input = document.getElementById("converter_input").value;
 
-	if (input == "") {
-		document.getElementById("converter_result").value = result;
+	input = input.replace(/,/g, '');
+
+	if (input == "" || input == null) {
+		document.getElementById("converter_result").value = null;
 		return;
 	}
 
-
-	result = Number(input) * Number(input);
+	// temp dummy code :	
+	result = Number(input) * Number(input);	
 
 	//get the units the user picked.
-
 	var from = document.getElementById("from_unit").value;
 	var to = document.getElementById("to_unit").value;
 
-	console.log(from);
-	console.log(to);
+	// do conversion;
 	
-	// do some magic to convert it based on the exising units
+	if (isNaN(result) || result == null || result == undefined) {		
+		document.getElementById("converter_result").value = null;
+		return;
+	}	
 
-	
-	document.getElementById("converter_result").value = result;
+	result = result.toFixed(conversion_precision);
+	result = Number(result);
+
+	document.getElementById("converter_result").value = result + " " + to;	
 }
 
 

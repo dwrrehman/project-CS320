6dc9093bb846a14b8b7294dd5cce0573ba2c20d8
Jake Palmer <jake.palmer1842@gmail.com>
2018-12-01 13:13:20 -0800

    Removed old view switching code from base.js

---
+++ app/imports/api/systems/systems.js
@@ -0,0 +1,8 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+const Systems = new Mongo.Collection('Systems');
+
+const SystemsSchema = new SimpleSchema({
+
+});

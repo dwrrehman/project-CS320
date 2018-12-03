8d49360bbb43443d7da301586e24ee7b2b454fc3
Daniel Rehman <dwrrehman@gmail.com>
2018-12-03 14:18:12 -0800

    add system button for ui, make selector populate from database, and finish the bulk of the ui.

--- app/imports/ui/components/calculator/calculator.js
+++ app/imports/ui/components/calculator/calculator.js
@@ -1,2 +1,10 @@
 import './calculator.html';
 import '../../stylesheets/calculator.css';
+import { Template } from 'meteor/templating';
+import { Systems } from '../../../api/systems/systems.js'
+
+Template.calculator.helpers({
+	SystemsCollection() {
+		return Systems.find();
+	}
+});

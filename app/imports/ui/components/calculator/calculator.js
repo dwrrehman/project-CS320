d08c97613b4ff8d8120e032d13c686ffd1b0fab5
Jake Palmer <jake.palmer1842@gmail.com>
2018-12-05 12:41:11 -0800

    Added event handler to calculator.js

--- app/imports/ui/components/calculator/calculator.js
+++ app/imports/ui/components/calculator/calculator.js
@@ -14,4 +14,24 @@ Template.calculator.helpers({
   systemsCollection() {
     return Systems.find();
   },
+  receive(string) {
+    let displayString = '';
+
+    if (string.length === 1) {
+      displayString += string;
+    } else {
+      if (string === 'clear') {
+        displayString = '';
+      } else {
+        displayString = '';
+      }
+    }
+    return displayString;
+  },
+});
+
+Template.calculator.events({
+  'click .clearbutton'(event) {
+    console.log('SUCCESS');
+  },
 });

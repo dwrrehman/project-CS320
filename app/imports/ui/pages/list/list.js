905cf6b497287898924bf733433ceb0de9730e22
Jake Palmer <jake.palmer1842@gmail.com>
2018-12-06 02:17:29 -0800

    Display compound units in unit list

--- app/imports/ui/pages/list/list.js
+++ app/imports/ui/pages/list/list.js
@@ -1,15 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { BaseUnits } from '../../../api/base/base.js';
+import { compoundUnits } from '../../../api/compound/compound.js';
 
 import './list.html';
 
 Template.list.onCreated(function listOnCreated() {
   Meteor.subscribe('BaseUnits');
+  Meteor.subscribe('CompoundUnits');
 });
 
 Template.list.helpers({
   baseUnitsList() {
     return BaseUnits.find();
+  },
+
+  compoundUnitsList() {
+    return compoundUnits.find();
   },
 });
